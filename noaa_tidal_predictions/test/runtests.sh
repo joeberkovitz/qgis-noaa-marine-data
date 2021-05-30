9e331d3337223c9e21cf7ef637cf53e074dd3874
joeberkovitz <joseph.berkovitz@gmail.com>
2021-05-30 17:28:36 -0400

    remove duplicated specification of PYTHONPATH

--- noaa_tidal_predictions/test/runtests.sh
+++ noaa_tidal_predictions/test/runtests.sh
@@ -7,7 +7,7 @@ export GDAL_DRIVER_PATH=${QGIS_PREFIX_PATH}/lib/gdalplugins
 export GDAL_DATA=${QGIS_BUNDLE}/Resources/gdal
 export PROJ_LIB=${QGIS_BUNDLE}/Resources/proj
 export PYQGIS_STARTUP=pyqgis-startup.py
-export PYTHONHOME=/Applications/QGIS-LTR.app/Contents/MacOS
+export PYTHONHOME=${QGIS_PREFIX_PATH}
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 # Note: this relies on nose having been installed in ${QGIS_PREFIX_PATH}/bin by the pip binary bundled with QGIS.
 nosetests $*
